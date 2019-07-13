f2943b930a254efceabb7e0587349c446082bade
Daniel Stahl <danstahl1138@gmail.com>
2019-07-13 15:05:57 -0500

    cleanup

--- example_allais.R
+++ example_allais.R
@@ -15,53 +15,41 @@ p_combined=apply(p_realworld, 1, prod)
 compute_expectation=function(state_space, p, func=function(x){x}){
   rbind(p)%*%func(as.matrix(state_space))
 }
-
+utility=function(x){log(100000+1000000*x)}
 raw_expectation=compute_expectation(state_space, p_combined)
-log_utility_expectation_init=compute_expectation(state_space, p_combined, function(x){log(0.1+x)})
+log_utility_expectation_init=compute_expectation(state_space, p_combined, utility)
 
-q=c(3, 10, 1, 6, 1, 2, 9, 1, 1, 4, 5, 1)
-q_combined=q/sum(q)
+#q=c(3, 10, 1, 6, 1, 2, 9, 1, 1, 4, 5, 1)
+#q_combined=q/sum(q)
+q_combined=c(
+  0.0393972092,
+  0.1599723308, 
+  0.0208296422, 
+  0.0353572647, 
+  0.0718684143, 
+  0.1810057193, 
+  0.0002355861,
+  0.1270205081,
+  0.0678170439, 
+  0.1805296005,
+  0.0382197078,
+  0.0777469731
+)
+q_r=round(q_combined, 2)
+q_r[7]=.01
+q_r[11]=0.03
+q_r[12]=0.07
+sum(q_r)
 
-actual_utility_expectation=compute_expectation(state_space, q_combined, function(x){log(0.1+x)})
-
-
-### counterfactual
-
-m=t(cbind(c(-2, .5, .1), c(3, 2, 1)))
-b=c(2, 1)
-b_2=c(-2, 1)
-y=cbind(c(-5, 2))
-
-t(m)%*%y
-b%*%y
-
-library(pracma)
-
-rref(cbind(m, b))
-
-library(limSolve)
-
-G=diag(ncol(m))
-h=(1:ncol(m))*0+.000001
-
-ldei(m, b, G=G, H=h)
-ldei(m, b_2, G=G, H=h)
-
-m_works=t(cbind(c(2, .5, 2), c(3, 2, 1)))
-#b_works=c(2, 1)
-b_works=c(1.9, 1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-ldei(m_works, b_works, G=G, H=h)
-ldei(m_works, b_works)
-
-
-##experiment
-
-m2=t(cbind(c(-2, .5, 2), c(3, 2, 1)))
-#b_works=c(2, 1)
-b2=c(1.1, 1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-ldei(m_works, b_works, G=G, H=h)
-ldei(m_works, b_works)
-
-
-
-ldei(t(as.matrix(state_space)), c(3, 2, 1, .4), G=diag(12), H=(1:12)*0+.000001)
+actual_utility_expectation=compute_expectation(state_space, q_combined, utility)
+compute_expectation(state_space, q_r, utility)
+continue=T
+while(continue){
+  q_p=runif(length(p_combined))
+  q_p=q_p/sum(q_p)
+  res=compute_expectation(state_space, q_p, utility)
+  if(res[1]>res[2]&&res[3]<res[4]&&res[4]<res[1]){
+    continue=F
+    print(q_p)
+  }
+}
